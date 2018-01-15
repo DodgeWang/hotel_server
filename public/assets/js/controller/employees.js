eac046737357c8d4b65b3ffce031c49c6676e40d
user <user@userdeMacBook-Pro.local>
2018-01-15 20:43:32 +0800

    ..

--- public/assets/js/controller/employees.js
+++ public/assets/js/controller/employees.js
@@ -1,6 +1,14 @@
 $(function(){
 	$('#departmentSelect').change(function(){
-		alert($(this).val())
-		
+		// alert($(this).val())
+		$.get(
+			"/api/employee/list", 
+			{ 
+				departmentId: $(this).val()
+		    },
+            function(data){
+              console.log("Data Loaded: " + data);
+            }
+        );
 	})
 })
