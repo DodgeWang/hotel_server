c757f8431e9e97796d3989064f73843c711dd817
user <user@userdeMacBook-Pro.local>
2018-01-15 21:06:16 +0800

    ..

--- public/assets/js/controller/employees.js
+++ public/assets/js/controller/employees.js
@@ -1,13 +1,36 @@
 $(function(){
 	$('#departmentSelect').change(function(){
-		// alert($(this).val())
 		$.get(
 			"/api/employee/list", 
 			{ 
-				departmentId: $(this).val()
+			  departmentId: $(this).val()
 		    },
             function(data){
-              console.log("Data Loaded: " + data);
+               var newDom = '';
+               for(var i = 0; i < data.datalist.length; i++){
+               	  var itermDom = '<tr class="even pointer">\
+                                      <td class="a-center ">\
+                                        <input type="checkbox" class="flat" name="table_records">\
+                                      </td>\
+                                      <td>{{EmployeeInfo.name}}</td>\
+                                      <td>{{username}}</td>\
+                                      <td>{{Department.name}}</td>\
+                                      <td>{{Role.name}}</td>\
+                                      <td>{{EmployeeInfo.phone}}</td>\
+                                      <td>\
+                                        <button type="button" class="btn btn-success btn-xs">正常</button>\
+                                      </td>\
+                                      <td style="position: relative;">\
+                                          <a href="editEmployee.html" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> 编辑 </a>\
+                                          <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> 删除 </a>\
+                                          <a href="#" class="btn btn-primary btn-xs"><i class="fa fa-key"></i> 重置密码 </a>\
+                                      </td>\
+                                    </tr>'
+                   newDom += itermDom;
+                   console.log(newDom)
+               }
+               $("#datalistBox").html(newDom)
+               console.log("Data Loaded: " + data.datalist);
             }
         );
 	})
